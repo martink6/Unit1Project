84e37fb485352c8d8e6447e3cb04170ff018a8f1
BT_2N2_17 <sample@bths.edu>
2023-09-21 13:39:08 -0400

    Almost done

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         String name = askQuestion("What is your name? ");
-        System.out.printf("Welcome %s!", name);
+        System.out.printf("Welcome %s!%n", name);
         int partySize = (int) stringToDouble(askQuestion("How many people are in your party? "));
+        double tipPercent = stringToDouble(askQuestion("How much do you want to tip? (in percent) "));
+        double[] itemCosts = getItems();
+        double total = 0;
+        for (double itemCost : itemCosts) {
+            System.out.printf("Current total: %f, item being added: %f. %n", total, itemCost);
+            total += itemCost;
+        }
+
+        double tipCost = total * (tipPercent / 100);
+
+        System.out.printf("Your total tip cost is $%f.%n", tipCost);
+
+    }
+
+    static double[] getItems() {
+        System.out.println("Input the costs for each of your items. Input '0' or nothing to exit");
+        double itemCost = stringToDouble(askQuestion("Input an item cost >>>"));
+        List<Double> itemCosts = new ArrayList<>();
+        itemCosts.add(itemCost);
+        while (itemCosts.get(itemCosts.size() - 1) != 0) {
+            itemCosts.add(stringToDouble(askQuestion("Input an item cost >>>")));
+        }
+
+        double[] costs = new double[itemCosts.size()];
+
+        for (double cost: itemCosts) {
+            costs[itemCosts.indexOf(cost)] = cost;
+        }
+
+        return costs;
     }
 
     static String askQuestion(String question) {
@@ -17,7 +49,7 @@ public class Main {
         try {
             return Double.parseDouble(str);
         } catch (Exception e) {
-            System.out.printf("Failed to convert string to double: %s, using 0.", e);
+            System.out.println(e);
             return 0;
         }
     }
